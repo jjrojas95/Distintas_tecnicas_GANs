c4e185d96e4bc85bb7a6f3c97af2c8c0ce900ed4
Juan Jose Rojas Cortes <jjrojas95@gmail.com>
2020-08-07 19:53:27 -0500

    Contrucción de la capa Maxout y mejora del test de la capa Maxout, no da el resultado que se espera (mejorar el call method)

--- helper_func/custom_layers.py
+++ helper_func/custom_layers.py
@@ -1,5 +1,44 @@
 import tensorflow as tf
 from tensorflow.keras.layers import Layer
+from tensorflow.python.keras import backend as K
+from tensorflow.python.keras import initializers
+from tensorflow.python.keras import regularizers
 
 class Maxout(Layer):
-    pass
+    def __init__(self, 
+                units,k, kernel_initializer='glorot_uniform',
+                bias_initializer='zeros',
+                kernel_regularizer=None,
+                bias_regularizer=None, 
+                **kwargs):
+
+        super(Maxout, self).__init__(**kwargs)
+
+        self.units = int(units) if not isinstance(units, int) else units
+        self.k = int(k) if not isinstance(k, int) else k
+        self.kernel_initializer = initializers.get(kernel_initializer)
+        self.bias_initializer = initializers.get(bias_initializer)
+        self.kernel_regularizer = regularizers.get(kernel_regularizer)
+        self.bias_regularizer = regularizers.get(bias_regularizer)
+    
+    def build(self, input_shape):
+        self.kernel = self.add_weight(
+            "kernel", 
+            shape=[int(input_shape[-1]), self.units, self.k],
+            initializer=self.kernel_initializer,
+            regularizer=self.kernel_regularizer,
+            dtype=self.dtype,
+            trainable=True)
+
+        self.bias = self.add_weight(
+            "bias", 
+            shape=[self.units, self.k],
+            initializer=self.bias_initializer,
+            regularizer=self.bias_regularizer,
+            dtype=self.dtype,
+            trainable=True)
+    
+    def call(self, x):
+        z = tf.tensordot(x, self.kernel, [[-1], [0]]) + self.bias
+        h = tf.reduce_max(z, axis = -1)
+        return h
--- tests/maxout_test.py
+++ tests/maxout_test.py
@@ -1,32 +1,68 @@
 import unittest
 from unittest import TestCase
+import numpy as np
 import tensorflow as tf
 from tensorflow.keras.layers import Layer
+
 
 from helper_func.custom_layers import Maxout
 
 class TestMaxoutLayer(TestCase):
     def setUp(self):
         self.k = 5
-        self.dim = 240
-        self.maxout_instance = Maxout(input_shape = (784,))
+        self.units = 240
+        self.input_shape = (784,)
+        self.maxout_instance = Maxout(self.units, self.k)
+        self.maxout_instance.build(self.input_shape)
 
     def test_maxout_type(self):
         self.assertTrue(isinstance(self.maxout_instance, Layer))
     
     def test_kernel_shape(self):
-        shape = self.maxout_instance.kernel.shape.as_list()
-        self.assertEqual(shape, (None, input_shape[0], self.dim, self.k))
+        shape = self.maxout_instance.kernel.shape
+        self.assertEqual(shape, (self.input_shape[0], self.units, self.k))
     
     def test_bias_shape(self):        
-        shape = self.maxout_instance.bias.shape.as_list()
-        self.assertEqual(shape, (None, self.dim, self.k))
+        shape = self.maxout_instance.bias.shape
+        self.assertEqual(shape, (self.units, self.k))
     
-    def test_output_shape(self):        
-        self.assertEqual(self.maxout_instance.output_shape(), (None, self.dim))
+    def test_output_shape(self):
+        input_shape = (5,)
+        units = 10
+        k = 3
+        other_maxout = Maxout(units, k, input_shape=input_shape)
+        x = tf.ones((10,5))
+        output = other_maxout(x) 
+
+        self.assertEqual(output.shape, (10, units))
     
-    def test_output_shape(self):      
-        self.assertEqual(self.maxout_instance.output_shape(), (None, self.dim))
+    def test_output_2samples_1unit(self):
+        # Primera parte, una sola unidad dos ejemplos y reducción de k.
+        m = 2
+        input_shape = (2,)
+        units = 1
+        k = 3
+        w = tf.reshape(tf.cast(tf.range(1,7), tf.float32), (input_shape[-1], units, k))
+        other_maxout = Maxout(units, k)
+        other_maxout.build((m, *input_shape))
+        other_maxout.kernel.assign(w)
+        x = tf.reshape(tf.cast(tf.range(1,5), tf.float32), (m, *input_shape))
+        expect_val = tf.reshape(tf.convert_to_tensor([15., 33.]), (m, units))
+        self.assertEqual(other_maxout(x).numpy().tolist(), expect_val.numpy().tolist())
+
+    def test_output_1samples_2unit(self):
+        m = 1
+        input_shape = (2,)
+        units = 2
+        k = 3
+        w = tf.reshape(tf.cast(tf.range(1,13), tf.float32), (input_shape[-1], units, k))
+        other_maxout = Maxout(units, k)
+        other_maxout.build((m, *input_shape))
+        other_maxout.kernel.assign(w)
+        x = tf.reshape(tf.cast(tf.range(1,3), tf.float32), (m, *input_shape))
+        expect_val = tf.reshape(tf.convert_to_tensor([21., 30.]), (m, units))
+        self.assertEqual(other_maxout(x).numpy().tolist(), expect_val.numpy().tolist())
+
 
 if __name__ == '__main__':
     unittest.main()
